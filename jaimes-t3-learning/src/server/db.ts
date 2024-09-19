cc891b1cbf84f044d5e86886dedd3ed58d70b0bd
JaimesJames <s.kaewkulsri1212@gmail.com>
2024-09-19 23:51:32 +0700

    commit

--- jaimes-t3-learning/src/server/db.ts
+++ jaimes-t3-learning/src/server/db.ts
@@ -1,6 +1,7 @@
 import { Client } from "@planetscale/database";
 import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 import { env } from "~/env";
 
@@ -10,7 +11,6 @@ const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    adapter: new PrismaPlanetScale(psClient),
   });
 
 const globalForPrisma = globalThis as unknown as {
